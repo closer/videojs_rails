5764be8d7d2d63e71d1a4188f138022f4708b689
Eido NABESHIMA <closer009@gmail.com>
2012-10-22 11:44:00 +0900

    typo

--- lib/videojs_rails/view_helpers.rb
+++ lib/videojs_rails/view_helpers.rb
@@ -3,6 +3,7 @@ module VideojsRails
     def videojs_rails(*options)
       @options = options.extract_options!
       @options[:classes] ||= 'vjs-default-skin'
+      @options[:types] ||= [:mp4]
       render "videojs_rails/videojs_rails"
     end
   end
